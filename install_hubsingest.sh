0435e799976bb37d28981b35bc2e519b4a619656
Alexandru Mahmoud <almahmoudresearch@gmail.com>
2025-01-08 12:15:30 -0500

    Scripts paths

--- install_hubsingest.sh
+++ install_hubsingest.sh
@@ -3,8 +3,8 @@ HUBSINGESTPATH=${BIOC_HUBSINGEST_PATH:-/usr/local/bin/hubsingest}
 mkdir -p $HUBSINGESTPATH
 
 curl -o $HUBSINGESTPATH/hubsingest https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/hubsingest
-curl -o $HUBSINGESTPATH/hubsingest_create_endpoint.sh https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/hubsingest_create_endpoint.sh
-curl -o $HUBSINGESTPATH/hubsingest_delete_endpoint.sh https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/hubsingest_delete_endpoint.sh
+curl -o $HUBSINGESTPATH/hubsingest_create_endpoint.sh https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/scripts/hubsingest_create_endpoint.sh
+curl -o $HUBSINGESTPATH/hubsingest_delete_endpoint.sh https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/scripts/hubsingest_delete_endpoint.sh
 
 chmod +x $HUBSINGESTPATH/hubsingest
 echo 'In order to persist the path, you may want to run, and/or add to your `rc` files:'
