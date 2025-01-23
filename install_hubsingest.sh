afd43b7f85715c557474a6ed084e6f639e7e3c63
Alexandru Mahmoud <almahmoudresearch@gmail.com>
2025-01-23 15:00:37 -0500

    Better install

--- install_hubsingest.sh
+++ install_hubsingest.sh
@@ -2,12 +2,25 @@
 HUBSINGESTPATH=${BIOC_HUBSINGEST_PATH:-/usr/local/bin/hubsingest}
 mkdir -p $HUBSINGESTPATH/scripts
 
+# Download main script
 curl -o $HUBSINGESTPATH/hubsingest https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/hubsingest
-curl -o $HUBSINGESTPATH/scripts/hubsingest_create_endpoint.sh https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/scripts/hubsingest_create_endpoint.sh
-curl -o $HUBSINGESTPATH/scripts/hubsingest_delete_endpoint.sh https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/scripts/hubsingest_delete_endpoint.sh
-curl -o $HUBSINGESTPATH/scripts/hubsingest_test_endpoint.sh https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/scripts/hubsingest_test_endpoint.sh
-
 chmod +x $HUBSINGESTPATH/hubsingest
+
+# Download all script files
+SCRIPTS=(
+  "hubsingest_create_endpoint.sh"
+  "hubsingest_delete_endpoint.sh"
+  "hubsingest_test_endpoint.sh"
+  "hubsingest_scan_data.sh"
+  "hubsingest_launch_rstudio.sh"
+  "reset_endpoint_ssl.sh"
+)
+
+for script in "${SCRIPTS[@]}"; do
+  curl -o "$HUBSINGESTPATH/scripts/$script" "https://raw.githubusercontent.com/Bioconductor/hubsingest/refs/heads/devel/scripts/$script"
+  chmod +x "$HUBSINGESTPATH/scripts/$script"
+done
+
 echo 'In order to persist the path, you may want to run, and/or add to your `rc` files:'
 echo "export PATH=\"\$PATH:$HUBSINGESTPATH\""
 
